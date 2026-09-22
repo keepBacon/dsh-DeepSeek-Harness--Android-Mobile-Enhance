386a2698c34ad8ad36e6b3175401fe964599e2a0
keepBacon <PardaShannon7275@outlook.com>
2026-09-22 21:31:57 +0800

    Update V0.1.1 versionCode assertions

--- scripts/check-fs-search-fix.sh
+++ scripts/check-fs-search-fix.sh
@@ -16,7 +16,7 @@ must "$P" 'ripgrepResolverOverrideReady()'
 must "$BT" 'validate_ripgrep_runtime()'
 must "$BT" 'validate_ripgrep_runtime "$stage"'
 must "$BG" 'versionName = "0.1.1"'
-must "$BG" 'versionCode = 35'
+must "$BG" 'versionCode = 36'
 must "$MA" 'DSH-Android/0.1.1'
 must "$BT" '"app": "0.1"'
 node --check "$P" >/dev/null
